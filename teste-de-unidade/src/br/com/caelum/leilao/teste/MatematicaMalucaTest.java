224fa8570cc5e12a9cdebbc1fecd00ea5d447a3d
tls <tls@Machado-PC>
2018-09-01 13:59:10 -0300

    update Aula_02 completa_

---
+++ teste-de-unidade/src/br/com/caelum/leilao/teste/MatematicaMalucaTest.java
@@ -0,0 +1,32 @@
+package br.com.caelum.leilao.teste;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import br.com.caelum.leilao.dominio.MatematicaMaluca;
+
+public class MatematicaMalucaTest {
+	
+	
+//	=========  3 Teste 
+
+    @Test
+    public void deveMultiplicarNumerosMaioresQue30() {
+        MatematicaMaluca matematica = new MatematicaMaluca();
+        assertEquals(50*4, matematica.contaMaluca(50));
+    }
+
+    @Test
+    public void deveMultiplicarNumerosMaioresQue10EMenoresQue30() {
+        MatematicaMaluca matematica = new MatematicaMaluca();
+        assertEquals(20*3, matematica.contaMaluca(20));
+    }
+
+    @Test
+    public void deveMultiplicarNumerosMenoresQue10() {
+        MatematicaMaluca matematica = new MatematicaMaluca();
+        assertEquals(5*2, matematica.contaMaluca(5));
+    }
+
+}
