5de03c60c6357c6e6712cbc654e0552e053f08e4
tls <tls@Machado-PC>
2018-09-01 11:23:28 -0300

    _add Class Palindromo _bug corrigido

--- teste-de-unidade/src/br/com/caelum/leilao/dominio/Palindromo.java
+++ teste-de-unidade/src/br/com/caelum/leilao/dominio/Palindromo.java
@@ -8,11 +8,18 @@ public class Palindromo {
 	                .toUpperCase().replace(" ", "").replace("-", "");
 
 	        for(int i = 0; i < fraseFiltrada.length(); i++) {
-	            if(fraseFiltrada.charAt(i) != 
-	                    fraseFiltrada.charAt(fraseFiltrada.length() -(i+1))) {
+//	            if(fraseFiltrada.charAt(i) != 
+//	                    fraseFiltrada.charAt(fraseFiltrada.length() -(i+1))) {
+//	        ------------------------------------------------------------------------------	
+	       /*** O bug está justamente na hora de pegar o caractere do fim da frase.
+	        		Precisamos subtrair 1, afinal o array vai de 0 até tamanho da string -1. ***/
+	            // bug corrigido na linha abaixo!
+	            int outroLado = fraseFiltrada.length() -i - 1;
+	            if(fraseFiltrada.charAt(i) != fraseFiltrada.charAt(outroLado)) {
+//	        -------------------------------------------------------------------------------
 	                return false;
 	            }
-	        }
+	        }  
 
 	        return true;
 	    }
